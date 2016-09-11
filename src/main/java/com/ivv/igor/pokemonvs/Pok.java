7ba8fa19643f299fbcb4d46808a9ec1283cf8384
Igor <ziar.igor@gmail.com>
2016-09-11 13:26:46 +0300

    begin

--- src/main/java/com/ivv/igor/pokemonvs/Pok.java
+++ src/main/java/com/ivv/igor/pokemonvs/Pok.java
@@ -7,7 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class Pok extends AppCompatActivity {
     int px=0;
@@ -17,17 +19,25 @@ public class Pok extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pok);
-        ImageView im=(ImageView)findViewById(R.id.im);
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(getResources(), R.drawable.pkm_full, options);
+        int imageHeight = options.outHeight;
+        int imageWidth = options.outWidth;
+        String imageType = options.outMimeType;
         Bitmap poks10 = BitmapFactory.decodeResource(getResources(), R.drawable.pkm_full);
         px=poks10.getWidth()/10;
+        LinearLayout linearLayout=(LinearLayout)findViewById(R.id.ll);
         for(int i=0;i<151;i++){
             p[i]=Bitmap.createBitmap(poks10,0+i%10*px,0+px*i/10,px,px);
+            ImageView imageView=new ImageView(this);
+            imageView.setImageBitmap(p[i]);
+            imageView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            linearLayout.addView(imageView);
         }
 
-        im.setImageBitmap(p[0]);
+
     }
-    public void changeImage(View view){
-        ImageView im=(ImageView)findViewById(R.id.im);
-        im.setImageBitmap(p[k++]);
-    }
+
 }
